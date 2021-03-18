46d81af64b85174dea0018a3faf3787236ad696f
NormanPen <norman.pendzich@outlook.de>
2021-03-18 20:26:23 +0000

    Hintergrundbild in Arbeit. Braucht noch Anpassung

--- src/sites/About/About.js
+++ src/sites/About/About.js
@@ -1,26 +1,37 @@
-import Kontaktfooter from '../../components/Footer/KontaktFooter';
+import Kontaktfooter from "../../components/Footer/KontaktFooter";
+import "./About.scss";
 
-function 
-About() {
-    return (
-        <div>
-            <h1>Über Uns</h1>
-            <p>Margot Döpper wollte nicht nur Landwirtin sein. Heute organisiert sie, unterstützt von ihrem Mann Hans Döpper, Kindergeburtstage auf dem Bauernhof. In einem Kurs der Landwirtschaftskammer erwarb sie das nötige Know-how. </p>
-            <button>Jetzt entdecken</button>
+function About() {
+  return (
+    <div className="about">
+      <h1>Über Uns</h1>
+      <p>
+        Margot Döpper wollte nicht nur Landwirtin sein. Heute organisiert sie,
+        unterstützt von ihrem Mann Hans Döpper, Kindergeburtstage auf dem
+        Bauernhof. In einem Kurs der Landwirtschaftskammer erwarb sie das nötige
+        Know-how.{" "}
+      </p>
+      <button>Jetzt entdecken</button>
 
-            <h2>Willkommen</h2>
-            <p>Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende Generationen in diesem Umfang zu erhalten.
+      <h2>Willkommen</h2>
+      <p>
+        Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor
+        Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der
+        "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende
+        Generationen in diesem Umfang zu erhalten. Seit 2007 gibt es nun die
+        Erlebniswelt Bauernhof. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+        justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
 
-            Seit 2007 gibt es nun die Erlebniswelt Bauernhof. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-
-            <h3>Der Hof</h3>
-            <p>Auf dem Bauernhof in der 4. Generatinon gibt es viel zu entdecken.</p>
-            <button>Zu unserem Programm</button>
-            <Kontaktfooter></Kontaktfooter>
-
-            
-        </div>
-    );
+      <h3>Der Hof</h3>
+      <p>Auf dem Bauernhof in der 4. Generatinon gibt es viel zu entdecken.</p>
+      <button>Zu unserem Programm</button>
+      <Kontaktfooter></Kontaktfooter>
+    </div>
+  );
 }
 
 export default About;
--- src/sites/Home/Home.js
+++ src/sites/Home/Home.js
@@ -1,49 +1,60 @@
-import Kontaktfooter from '../../components/Footer/KontaktFooter';
-import TeaserMain from '../../components/TeaserMain/TeaserMain';
+import Kontaktfooter from "../../components/Footer/KontaktFooter";
+import TeaserMain from "../../components/TeaserMain/TeaserMain";
 
-
-
-
+import "./Home.scss";
 
 function Home() {
-    return (
-        <div>
-            <TeaserMain
-                class="yellow"
-                ueberschrift="Herzlich Wilkommen auf unserem Hof"
-                btnname="Jetzt entdecken"
-                target="/Kontakt"
-            >
-                Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem.
-            </TeaserMain>
-            <TeaserMain ueberschrift="Teaser">
-                Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem.{<button>Klick mich</button>}
-            </TeaserMain>
+  return (
+    <div className="home">
+      <TeaserMain
+        class="yellow"
+        ueberschrift="Herzlich Wilkommen auf unserem Hof"
+        btnname="Jetzt entdecken"
+        target="/Kontakt"
+      >
+        Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem.
+      </TeaserMain>
+      <TeaserMain ueberschrift="Teaser">
+        Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem.
+        {<button>Klick mich</button>}
+      </TeaserMain>
 
+      <h2>Willkommen</h2>
+      <p>
+        Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor
+        Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der
+        "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende
+        Generationen in diesem Umfang zu erhalten.
+      </p>
 
+      <h3>Margot & Hans Döpper</h3>
+      <p>
+        Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor
+        Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der
+        "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende
+        Generationen in diesem Umfang zu erhalten.
+      </p>
+      <button>Über Uns</button>
 
+      <h3>Spielend lernen</h3>
+      <p>
+        Like the name of this Barley Lemon Bar and her appeal, fresh and
+        exciting are what she wants you and your skin to feel after spending a
+        pleasurable time in the shower.
+      </p>
+      <button>Zum Programm</button>
 
+      <h2>Unsere Tiere</h2>
+      <p>
+        In this reinvented flick, which hovers like a Matisse cutout between the
+        lashline and brow bone, curved crease of the eyelid, while the top is a
+        long, flat line that extends past the outer corners.
+      </p>
+      <button>Tiere kennenlernen</button>
 
-
-
-            <h2>Willkommen</h2>
-            <p>Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende Generationen in diesem Umfang zu erhalten.</p>
-
-            <h3>Margot & Hans Döpper</h3>
-            <p>Erlebniswelt Bauernhof ist gegründet worden um: den Kindern direkt vor Ort die Natur nahezubringen, um Ihnen ein Gefühl für die Schönheit der "Schöpfung" zu vermitteln und die Welt auch noch für nachkommende Generationen in diesem Umfang zu erhalten.</p>
-            <button>Über Uns</button>
-
-            <h3>Spielend lernen</h3>
-            <p>Like the name of this Barley Lemon Bar and her appeal, fresh and exciting are what she wants you and your skin to feel after spending a pleasurable time in the shower.</p>
-            <button>Zum Programm</button>
-
-            <h2>Unsere Tiere</h2>
-            <p>In this reinvented flick, which hovers like a Matisse cutout between the lashline and brow bone, curved crease of the eyelid, while the top is a long, flat line that extends past the outer corners.</p>
-            <button>Tiere kennenlernen</button>
-
-            <Kontaktfooter></Kontaktfooter>
-        </div>
-    );
+      <Kontaktfooter></Kontaktfooter>
+    </div>
+  );
 }
 
 export default Home;
