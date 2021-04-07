7f9defeebb2a7fd1dd8212cea0d1c338d10536e1
NormanPen <norman.pendzich@outlook.de>
2021-04-07 08:04:32 +0000

    Neue Seite Impressum und neue Komponenten Teaser +

--- src/components/TeaserMain/TeaserMain.js
+++ src/components/TeaserMain/TeaserMain.js
@@ -4,6 +4,7 @@ import "./TeaserMain.scss";
 function TeaserMain(props) {
   return (
     <div className="teaser-main">
+      <img src={props.img} alt={props.imgAlt} className="img-teaser-main" />
       <h1>{props.ueberschrift}</h1>
       <p>{props.children}</p>
       <Link to={props.target}>
